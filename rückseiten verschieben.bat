784661a17aef4a47f87da0dd1374104e448b5b3f
fuch <placek@posteo.de>
2013-12-07 22:05:59 +0100

    added simple user input

--- rückseiten verschieben.bat
+++ rückseiten verschieben.bat
@@ -1,17 +1,29 @@
 @echo off
+
+:start
+cls
+set foldername
+set filetype
 set count=0
+
+cls
+SET/P foldername=Please enter Foldername: 
+cls
+SET/P filetype=Please enter filetype: 
+cls
+
 for /d %%a in (*.*) do (
-	if not exist "%%a\r0ckseiten" (
-		md %%a\r0ckseiten
+	if not exist "%%a\%foldername%" (
+		md %%a\%foldername%
 		set /a count = count+1 
-		echo r0ckseiten ordner erstellt in %%a
+		echo %foldername% folder created in %%a
 		)
 )
 
 for /d %%a in (*.*) do (
-	if exist "%%a\r0ckseiten" (
-		move %%a\*r.png %%a\r0ckseiten\
-		echo dateien verschoben in %%\r0ckseiten\
+	if exist "%%a\%foldername%" (
+		move %%a\*r.%filetype% %%a\%foldername%\
+		echo dateien verschoben in %%\%foldername%\
 	)
 )
 echo Alles Fertig
